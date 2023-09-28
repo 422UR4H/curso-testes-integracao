d782859e2fdbac4acc37e267deed8f9e3858f114
422UR4H <tiagosindra@id.uff.br>
2023-09-27 23:38:37 -0300

    :white_check_mark: fix/test

--- aula-testes-05/tests/app.test.ts
+++ aula-testes-05/tests/app.test.ts
@@ -1,5 +1,4 @@
 import supertest from "supertest";
-
 import app from "./../src/app";
 
 const api = supertest(app);
@@ -10,21 +9,34 @@ describe("API test", () => {
     expect(status).toBe(200);
     expect(text).toBe("OK!");
   });
+});
 
-  it("should return fibonacci sequence when ask /fibonacci", async () => {
+describe("/fibonacci", () => {
+  it("should return an array and status 200 when parameter is a valid number", async () => {
+    const { status, body } = await api.get(`/fibonacci?elements=10`)
+    expect(status).toBe(200);
+    expect(body).toHaveLength(10);
+    expect(body).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+  });
+
+  it("should return status 400 when parameter is not send", async () => {
     expect((await api.get("/fibonacci")).status).toBe(400);
+  });
 
-    const stackOverflowIndex = (Number.MAX_VALUE + 1).toString();
-    const arr: Array<string> = ["a1", "-1", "0.99", "0", stackOverflowIndex];
+  it("should return status 400 when parameter is not send", async () => {
+    const stackOverflowIndex = (Number.MAX_VALUE + Number.MIN_VALUE).toString();
+    const arr: Array<string> = ["NaN", "a1", "-1", "0", "0.99", stackOverflowIndex];
     for (let i = 0; i < arr.length - 1; i++) {
       expect((await api.get(`/fibonacci?elements=${arr[i]}`)).status).toBe(400);
     }
-    expect((await api.get(`/fibonacci?elements=10`)).status).toBe(200);
   });
 });
 
 describe.skip("can be implemented", () => {
   it("should return status 400", async () => {
-    expect((await api.get("/fibonacci?elements=1.1")).status).toBe(400);
+    const arr: Array<string> = ["", "1", "1.1"];
+    for (let i = 0; i < arr.length; i++) {
+      expect((await api.get(`/fibonacci?elements=${arr[i]}`)).status).toBe(400);
+    }
   });
 });
